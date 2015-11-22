2c16773403000ad5e44e74c92e78a659ee98d050
Shaanjot Gill <sgill@pio.carrollu.edu>
2015-11-22 13:30:40 -0600

    Shaan Folder

---
+++ Shaan-java/ChocAn/src/project/Test.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Test {
+
+	public static void main(String[] args) {
+		SPReportProcess ver1 = new SPReportProcess("group5","!Tech!612");
+		ver1.computeReport(10, "2015-11-11");
+		ver1.printReport();
+		ver1.saveReport();
+		ver1.termConnection();
+	}
+
+}
